a5ec7bbbaefd7d28bbc4ddfeea9d3a8badef1c84
Oleksandr Dykun <sashaluckyman@gmail.com>
2019-02-19 21:51:09 -0800

    created reuseble redux form and edit component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -68,6 +68,7 @@ export const editStream = (id, formValues) => async dispatch => {
         type: EDIT_STREAM,
         payload: resp.data
     })
+    history.push('/');
 }
 
 export const deleteStream = (id) => async dispatch => {
